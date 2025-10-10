92dc2e469ab3d4b019656e276fc204d0c64630c0
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2025-10-10 21:23:22 +0200

    feat: add data loaders for las and csv data

---
+++ src/data_loader.py
@@ -0,0 +1,67 @@
+"""
+This module provides functions to load data from las and CSV files.
+"""
+import os
+import laspy
+import label, utils, point_cloud
+
+def load_las_files_from_directory(directory_path, depth=1):
+    """
+    Load all .las files from the specified directory.
+
+    Parameters:
+    directory_path (str): The path to the directory containing .las files.
+    depth (int): The depth of directory (1 if all las files are in the same directory,
+                 2 if las files are in subdirectories of directory_path).
+
+    Returns:
+    list: A list of laspy.LasData objects.
+    """
+
+    point_clouds = []
+    if depth == 1:
+        for filename in os.listdir(directory_path):
+            if filename.endswith('.las'):
+                file_path = os.path.join(directory_path, filename)
+                las_data = laspy.read(file_path)
+                pc = point_cloud.PointCloud(las_data)
+                point_clouds.append(pc)
+    elif depth == 2:
+        for root, dirs, files in os.walk(directory_path):
+            for dir in dirs:
+                dir_path = os.path.join(root, dir)
+                for filename in os.listdir(dir_path):
+                    if filename.endswith('.las'):
+                        file_path = os.path.join(dir_path, filename)
+                        las_data = laspy.read(file_path)
+                        pc = point_cloud.PointCloud(las_data)
+                        point_clouds.append(pc)
+    return point_clouds
+
+def load_labels_from_csv(csv_file_path, labels_csv_path):
+    """
+    Load labels from a CSV file.
+
+    Parameters:
+    csv_file_path (str): The path to the CSV file containing labels.
+    labels_csv_path (str): The path to the CSV file containing the reference label table.
+
+    Returns:
+    list: A list of Label objects (see label module).
+    """
+    labels = []
+    with open(csv_file_path, 'r') as file:
+        lines = file.readlines()
+        if lines[0].strip() != "#Mean longitude;Mean latitude;Mean altitude;SNR;stddev longitude;stddev latitude;stddev altitude;stddev SNR; label":
+            raise ValueError(f"Unexpected header format. Expected:\n'#Mean longitude;Mean latitude;Mean altitude;SNR;stddev longitude;stddev latitude;stddev altitude;stddev SNR; label', \ngot:\n'{lines[0].strip()}'")
+        for line in lines[1:]:  # Skip header
+            parts = line.strip().split(';')
+            if len(parts) != 9:
+                raise ValueError(f"Unexpected number of columns in line: {line.strip()}")
+            line_label = label.Label(
+                label=utils.get_label_index(parts[8], labels_csv_path),
+                geolocation=(float(parts[0]), float(parts[1]), float(parts[2])),
+                std_devs=(float(parts[4]), float(parts[5]), float(parts[6]))
+            )
+            labels.append(line_label)
+    return labels
